e149c50c322d192a25463a1484559d5dcb706f6e
Aarish <aarishm767@gmail.com>
2023-08-13 02:06:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/moviessurveyor/MoviesSurveyorApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviessurveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesSurveyorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesSurveyorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviessurveyor/MoviesSurveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviessurveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSurveyorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
